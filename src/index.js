61d58751957d8b8d5d28ea8adbe84b0c01186a5c
skanagui <s.kanagui@gmail.com>
2021-05-26 11:33:30 -0400

    fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-const example = () =>{
+fetch("https://bravenewcoin.p.rapidapi.com/asset?type=CRYPTO&status=ACTIVE", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "afc5e9bcfdmshea1cbc205c5a1b7p1e02ebjsnd042a705e062",
+		"x-rapidapi-host": "bravenewcoin.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then(cryptos => renderCryptos(cryptos))
 
-    console.log("Hello")
-};
+const renderCryptos = (cryptos) =>{
 
-example(); 
+    for( let crypto of cryptos)
+
+
+}
+
